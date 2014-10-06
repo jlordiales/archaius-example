2a5271694d40fd5d2855ab371e6be7e194159ba1
Jose Luis Ordiales Coscia <jlordiales@gmail.com>
2014-10-06 11:18:09 -0300

    Changing the default name that archaius expects for its property file

--- src/main/java/archaius/example/ApplicationConfig.java
+++ src/main/java/archaius/example/ApplicationConfig.java
@@ -4,6 +4,9 @@ import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
 
 public class ApplicationConfig {
+    static {
+        System.setProperty("archaius.configurationSource.defaultFileName", "customConfig.properties");
+    }
 
     public String getStringProperty(String key, String defaultValue) {
         final DynamicStringProperty property = DynamicPropertyFactory.getInstance().getStringProperty(key,
